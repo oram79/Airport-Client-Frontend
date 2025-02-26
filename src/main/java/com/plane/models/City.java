9fea0c5a8558aeb116e6b3988557f216079a8f59
Nicholas Sharpe <162132214+nsharpe1@users.noreply.github.com>
2025-02-26 10:26:41 -0330

    Update City.java

--- src/main/java/com/plane/models/City.java
+++ src/main/java/com/plane/models/City.java
@@ -1,4 +1,53 @@
 package com.plane.models;
 
+import java.util.List;
+
 public class City {
+    private long id;
+    private String name;
+    private String country;
+
+    private List<Airport> airports;
+
+    public City() {
+    }
+
+    public City(long id, String name, String country, List<Airport> airports) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.airports = airports;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
 }
