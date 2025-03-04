7bf8726096f647b5b080aa8f65aad6bda81aec67
Nicholas Sharpe <162132214+nsharpe1@users.noreply.github.com>
2025-03-04 19:11:56 -0330

    Update Aircraft.java

--- src/main/java/com/plane/domain/Aircraft.java
+++ src/main/java/com/plane/domain/Aircraft.java
@@ -1,22 +1,34 @@
 package com.plane.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Aircraft {
     private long aircraftId;
 
     private String model;
-    private String airline;
+    private String airlineName;
+    private int numberOfPassengers;
 
-    private List<Passenger> passengers;
+    private Airport airportId;
+
+    private Passenger passengerID;
+
+    private List<Airport> airports = new ArrayList<Airport>();
+
+    private List<Passenger> passengers = new ArrayList<>();
 
     public Aircraft() {
     }
 
-    public Aircraft(long aircraftId, String model, String airline, List<Passenger> passengers) {
+    public Aircraft(long aircraftId, String model, String airlineName, int numberOfPassengers, Airport airportId, Passenger passengerID, List<Airport> airports, List<Passenger> passengers) {
         this.aircraftId = aircraftId;
         this.model = model;
-        this.airline = airline;
+        this.airlineName = airlineName;
+        this.numberOfPassengers = numberOfPassengers;
+        this.airportId = airportId;
+        this.passengerID = passengerID;
+        this.airports = airports;
         this.passengers = passengers;
     }
 
@@ -36,12 +48,44 @@ public class Aircraft {
         this.model = model;
     }
 
-    public String getAirline() {
-        return airline;
+    public String getAirlineName() {
+        return airlineName;
     }
 
-    public void setAirline(String airline) {
-        this.airline = airline;
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public Airport getAirportId() {
+        return airportId;
+    }
+
+    public void setAirportId(Airport airportId) {
+        this.airportId = airportId;
+    }
+
+    public Passenger getPassengerID() {
+        return passengerID;
+    }
+
+    public void setPassengerID(Passenger passengerID) {
+        this.passengerID = passengerID;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
     }
 
     public List<Passenger> getPassengers() {
