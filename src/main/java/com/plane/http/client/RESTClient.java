6cee22c78f3fe7968ca2153dee2722d0384feebc
Nicholas Sharpe <162132214+nsharpe1@users.noreply.github.com>
2025-03-04 17:12:05 -0330

    Add files via upload

---
+++ src/main/java/com/plane/http/client/RESTClient.java
@@ -0,0 +1,90 @@
+package com.plane.http.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plane.domain.Airport;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RESTClient {
+    private String serverURL;
+    private HttpClient client;
+
+    public String getResponseFromHTTPRequest() {
+        String responseBody = "";
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+
+        try {
+            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode()!=200) {
+                System.out.println("Status Code: " + response.statusCode());
+            }
+
+            responseBody = response.body();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return responseBody;
+    }
+
+
+    public List<Airport> getAllAirports() {
+        List<Airport> airports = new ArrayList<Airport>();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+
+        try {
+            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode()==200) {
+                System.out.println("***** " + response.body());
+            } else {
+                System.out.println("Error Status Code: " + response.statusCode());
+            }
+
+            airports = buildAirportListFromResponse(response.body());
+
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        return airports;
+    }
+
+    public List<Airport> buildAirportListFromResponse(String response) throws JsonProcessingException {
+        List<Airport> airports = new ArrayList<Airport>();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        airports = mapper.readValue(response, new TypeReference<List<Airport>>(){});
+
+        return airports;
+    }
+
+    public String getServerURL() {
+        return serverURL;
+    }
+
+    public void setServerURL(String serverURL) {
+        this.serverURL = serverURL;
+    }
+
+    public HttpClient getClient() {
+        if (client == null) {
+            client  = HttpClient.newHttpClient();
+        }
+
+        return client;
+    }
+}
